f14f5579c3eaa62ebb56b5025897f8c6d26d8850
Bill Glick <bglick@illinois.edu>
2021-03-03 11:25:01 -0600

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_audit
+class profile_audit {
+
+  include ::profile_audit::qualys
+  include ::profile_audit::root_equivalence
+
+}
---
+++ manifests/vetting.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_audit::vetting
+class profile_audit::vetting {
+}
