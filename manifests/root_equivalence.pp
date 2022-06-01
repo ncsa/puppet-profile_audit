541102712d09dfa9a1edd3f1c80ab1a9812e0ea0
Bill Glick <bglick@illinois.edu>
2022-06-01 15:08:28 -0500

    SVCPLAN-1236: Enable specific repo in RHEL7 to support root_equivalence packages

--- manifests/root_equivalence.pp
+++ manifests/root_equivalence.pp
@@ -5,6 +5,10 @@
 # @param crons
 #   Hash of CRON entries for root equivalence reporting
 #
+# @param enable_yum_repo
+#   Optional yum repo that must be enabled for list of packages to be installed.
+#   This is needed to support enabling the rhel-7-server-optional-rpms repo for RHEL 7.
+#
 # @param files
 #   Hash of files (scripts, etc) used for root equivalence reporting
 #
@@ -18,7 +22,27 @@ class profile_audit::root_equivalence (
   Hash $crons,
   Hash $files,
   Array $packages,
+  Optional[String] $enable_yum_repo = '',
 ) {
+
+  # IF SET $enable_yum_repo THEN ENSURE THAT REPO IS ENABLED
+  if ( ! empty($enable_yum_repo) ) {
+    $exec_name="yum-enable-${enable_yum_repo}"
+    Package {
+      require => [
+        Exec[$exec_name],
+      ],
+    }
+    exec { $exec_name :
+      path    => [
+        '/bin',
+        '/usr/bin',
+      ],
+      command => "yum-config-manager --enable ${enable_yum_repo}",
+      onlyif  => "yum repolist disabled | grep '${enable_yum_repo}'",
+      unless  => "yum repolist enabled | grep '${enable_yum_repo}'",
+    }
+  }
 
   ensure_packages( $packages )
 
